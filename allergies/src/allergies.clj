e9bc93607ca48dcd8ee92e91d6836ce773d03e4d
Zhiqiang (Sherlock) Qiao <615526+sherlockq@users.noreply.github.com>
2020-03-12 17:57:09 +0000

    clean it

--- allergies/src/allergies.clj
+++ allergies/src/allergies.clj
@@ -8,26 +8,13 @@
    :chocolate    32
    :pollen       64
    :cats         128})
-
-(defn top-score-allergy-pair [score]
-  (last
-    (take-while
-      #(<= (val %) score)
-      allergies-score)))
-
-(defn check-allergy [[score existed-allergies] allergy]
-  (if (<= (val allergy) score)
-    [(- score (val allergy)) (conj existed-allergies allergy)]
-    [score existed-allergies]))
-
-(defn allergies [score]                                     ;; <- arglist goes here
-  (->> (reduce check-allergy [score []] (reverse allergies-score))
-       second
-       (sort-by val)
-       (map key)
-    )
-  )
+(defn get-allergy-check-fn [score]
+  (comp not zero? (partial bit-and score) val))
+(defn allergies [score]
+  (map key
+       (filter
+         (get-allergy-check-fn score)
+         allergies-score)))
 
 (defn allergic-to? [score allergy]                          ;; <- arglist goes here
-  (boolean (some #(= allergy %) (allergies score)))
-  )
+  (boolean (allergy (set (allergies score)))))
